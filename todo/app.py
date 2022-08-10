957ee2230fbd968b4a951904a9ef96d8c968bbb8
Jessica Jolly <jessicajolly4416@gmail.com>
2022-08-10 23:00:28 +0530

    Created the view in /api/v1 which lists all routes

--- todo/app.py
+++ todo/app.py
@@ -1,6 +1,7 @@
 #using cd move to folder todo and run command 'python -m flask run' to launch website
 from datetime import datetime 
 from flask import Flask
+from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -40,6 +41,27 @@ class User(db.Model):
         """On construction, set date of joining."""
         super().__init__(*args, **kwargs)
         self.date_joined = datetime.now() 
+
+"""printing the list of api's provided on application"""
+@app.route('/api/v1', methods=["GET"])
+def info_view():
+    """List of routes for this API."""
+    output = {
+        'info': 'GET /api/v1',
+        'register': 'POST /api/v1/accounts',
+        'single profile detail': 'GET /api/v1/accounts/<username>',
+        'edit profile': 'PUT /api/v1/accounts/<username>',
+        'delete profile': 'DELETE /api/v1/accounts/<username>',
+        'login': 'POST /api/v1/accounts/login',
+        'logout': 'GET /api/v1/accounts/logout',
+        "user's tasks": 'GET /api/v1/accounts/<username>/tasks',
+        "create task": 'POST /api/v1/accounts/<username>/tasks',
+        "task detail": 'GET /api/v1/accounts/<username>/tasks/<id>',
+        "task update": 'PUT /api/v1/accounts/<username>/tasks/<id>',
+        "delete task": 'DELETE /api/v1/accounts/<username>/tasks/<id>'
+    }
+    '''outpu is a dict which is converted to JSON format'''
+    return jsonify(output)
 
 """The app.route decorator decorates the first view function; 
 it can specify one of the routes used to access the application. """
